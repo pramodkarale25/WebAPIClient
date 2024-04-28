fd0768f1e2cd369bbf66054daf4605f8892a6594
pramodkarale25 <pramodkarale.karale21@gmail.com>
2024-04-28 16:27:18 +0530

    first commit

---
+++ WebAPIClient/HttpClientPage.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HttpClientPage.aspx.cs" Inherits="WebAPIClient.HttpClientPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#btnClear').click(function () {
+                ulEmployees.empty();
+            });
+        });
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    Username : <input type="text" id="txtUsername" />
+    Password : <input type="password" id="txtPassword" />
+    <br /><br />
+        <asp:Button ID="Button1" runat="server" Text="Get Employees" OnClick="btn_Click" />
+    <input id="btnClear" type="button" value="Clear" />
+    <ul id="ulEmployees" runat="server"></ul>
+    </form>
+</body>
+</html>
---
+++ WebAPIClient/HttpClientPage.aspx.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WebAPIClient
+{
+    public partial class HttpClientPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri("http://localhost:51026/api/Employees"),
+                Method = HttpMethod.Get,
+            };
+
+            HttpClient client = new HttpClient();
+            string UNPass = Convert.ToBase64String(Encoding.Default.GetBytes("AdminUser:123456"));
+            request.Headers.Add("Authorization", "Basic " + UNPass);
+            //Need to change the PORT number where your WEB API service is running
+            var result = client.SendAsync(request).Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                var JsonContent = result.Content.ReadAsStringAsync().Result;
+                List<Employee> empList = JsonConvert.DeserializeObject<List<Employee>>(JsonContent);
+                foreach (var emp in empList)
+                {
+                    ulEmployees.InnerHtml +="<li>Name = " + emp.Name + " Gender = " + emp.Gender + " Dept = " + emp.Dept + " Salary = " + emp.Salary+"</li>";
+                }
+            }
+        }
+
+
+    }
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Dept { get; set; }
+        public int Salary { get; set; }
+    }
+}
